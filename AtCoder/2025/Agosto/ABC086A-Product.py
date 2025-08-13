ec0abef17e24f444c34b9a6fbb13b50e26258ade
FerPaye01 <ferpaye438@gmail.com>
2025-08-13 00:00:20 -0500

    Solucion ABC086A

--- AtCoder/2025/Agosto/ABC086A-Product.py
+++ AtCoder/2025/Agosto/ABC086A-Product.py
@@ -1,6 +1,7 @@
 """
 Problem Statement
-AtCoDeer the deer found two positive integers, a and b. Determine whether the product of a and b is even or odd.
+AtCoDeer the deer found two positive integers, a and b. Determine whether 
+the product of a and b is even or odd.
 
 Constraints
 - 1 ≤ a, b ≤ 10000
@@ -29,3 +30,29 @@ Odd
 
 As 1 × 21 = 21 is odd, print Odd.
 """
+import sys
+def procesar_dato(tokens):
+    if len(tokens)<2:
+        return
+    entero_a = int(tokens[0])
+    entero_b = int(tokens[1])
+    if entero_a % 2 == 0 or entero_b % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+    
+def main():
+    if sys.stdin.isatty():
+        casos_prueba =[
+            ["3", "4"]
+        ]
+        for tokens in casos_prueba:
+            print(f"---Caso de prueba : {tokens} ---")
+            procesar_dato(tokens)
+            print()
+    else: 
+        data = sys.stdin.read().split()
+        procesar_dato(data)
+if __name__ == "__main__":
+    main()
+
